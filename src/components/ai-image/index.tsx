74c49b5cc5b4dc99e5ba0d6f8585f99ba0457c23
tjones4701 <tjones4701@gmail.com>
2023-06-14 13:14:23 +1000

    More fixes

--- src/components/ai-image/index.tsx
+++ src/components/ai-image/index.tsx
@@ -1,26 +1,11 @@
 import { createImage } from "@/server/open-ai/ai";
-import { sleep } from "@/utilities/sleep";
 interface Props {
     children: string;
     className?: string;
 }
 
-
-export async function getImage(slide: string): Promise<string> {
-
-    const request = await fetch(`http://localhost:3000/api/images/${slide}`);
-    try {
-        return (await request?.json())?.src;
-    } catch (e) {
-        return "";
-    }
-}
-
 export const OpenAIImage: React.FC<Props> = async ({ children, className }) => {
-    const src = await getImage(children);
-    if (src == "") {
-        return null;
-    }
+    const src = await createImage(children);
     return (
         <img alt={children} className={className} src={src} />
     );
